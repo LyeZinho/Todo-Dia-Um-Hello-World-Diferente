4ad2e313068b1a4deaf710ee7a8d3fe635d64268
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2021-03-03 22:26:41 +0000

    Create helloworldcsharp.cs

---
+++ helloworldcsharp.cs
@@ -0,0 +1,18 @@
+/*
+hello world em csharp 
+
+c# e uma linguagem muito absurda e serio
+sem brincadeira ela e orientada a objeto
+e opera na .net muito bolada serve pra tudo
+desde game desing a OIT
+*/
+
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
