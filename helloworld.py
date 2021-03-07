3078a8a4057f038d5f6d08f1e5291680a00947ea
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2021-03-07 17:04:10 +0000

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+print("Hello, Wold")
+
+"""
+Achou que tinha mais coisa? 
+Não acabou e só isso mesmo
+Pythom e mo esquisito e torto ain ain indexação pqp
+linguagem chola porem mesmo que eu não goste de python
+temos que admitir python e muito rajada faz muita coisa
+e uma linguagem simple e faz tudo tem um bom sistema poo
+mesmo assim eu não gosto de python
+"""
