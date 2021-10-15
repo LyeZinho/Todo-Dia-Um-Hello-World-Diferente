81afeba8b1aefb245b7d3fe44e9e21fbab98f8e6
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2021-10-15 22:46:16 +0100

    Create helloworld.vb

---
+++ helloworld.vb
@@ -0,0 +1,7 @@
+'Vb por ser uma linguagem integrada com o .NET ela e bastante rica
+'Vb e muito auto explicativo mesmo que não pareça vb tambem e muito 
+'poderosa em termos de desenvolvimento geral
+
+sub program()
+  Console.WriteLine("Hello, World")
+End sub
