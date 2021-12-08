8b64202f973c8683eb22320c97aa89cab847b5e2
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2021-12-08 20:26:52 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,5 @@
 print("Hello, Wold")
 
 """
-Achou que tinha mais coisa? 
-Não acabou e só isso mesmo
-Pythom e mo esquisito e torto ain ain indexação pqp
-linguagem chola porem mesmo que eu não goste de python
-temos que admitir python e muito rajada faz muita coisa
-e uma linguagem simplese faz tudo tem um bom sistema poo
-mesmo assim eu não gosto de python
+Python e simplesmente absurdo e compacto!
 """
